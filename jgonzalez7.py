c4497ae6a7d99302d1409c51e5677990cc624531
Jovanny Gonzalez <jgonzalez1071@toromail.csudh.edu>
2025-02-26 02:26:09 +0000

    Lab 7

---
+++ jgonzalez7.py
@@ -0,0 +1,25 @@
+
+#Jovanny Gonzalez
+#CSC321-40
+
+x=29
+y=2.3
+z="Cookie"
+
+
+
+if x<30:
+    print("Variable x is less than 30")
+else:
+    print("Variable x is greater than or equal to 30")
+if y<2.7:
+    print("Variable y is less than 2.7")
+else:
+    print("Variable y is greater than or equal to 2.7")
+
+if z=="Cookie":
+    print("Variable z is equal to string Cookie")
+else:
+    print("Variable z is not equal to string Cookie")
+
+
