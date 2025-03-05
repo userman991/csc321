645c96e8c82a2dc30a28fa1ab721e7665cca772c
Jovanny Gonzalez <jgonzalez1071@toromail.csudh.edu>
2025-03-05 02:40:43 +0000

    Lab 8

---
+++ jgonzalez8.py
@@ -0,0 +1,27 @@
+#Jovanny Gonzalez
+
+import random
+
+randNum=random.randint(0,9)
+x=0
+y=0
+
+print("I picked a number from 0-9, ")
+print("guess that number: ")
+
+y=int(input())
+print("Your choice is represented by the letter y.")
+
+if y<randNum:
+    print("y is less than randNum. You lost.")
+elif y>randNum:
+    print("y is greater than randNum. You lost.")
+else:
+    print("y is equal to randNum. You WON!!!")
+
+for i in range(y):
+    print("What do you get when you cross a shark with a snowman? Frostbite!")
+
+while x<4:
+    print("Favorite Food: Tacos")
+    x=x+1
